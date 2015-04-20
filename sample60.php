c3bbd8d48d2a3dc34a0a8aea5db1dd9a0b1a4d6a
Shingo Yanai <yanai@photocreate.co.jp>
2015-04-20 17:53:03 +0900

    [chap3 p60]整数

---
+++ sample60.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yanai
+ * Date: 2015/04/20
+ * Time: 17:43
+ */
+$int1 = PHP_INT_MAX;
+$int2 = PHP_INT_MAX + 1;
+$int3 = intval("1");
+$int4= (int)"1";
+
+echo $int3; PHP_EOL;
+
