04f954cde5d9ec859638a07bf12fb17ee5ebdd32
Shingo Yanai <yanai@photocreate.co.jp>
2015-04-21 16:35:41 +0900

    [chap3 p85]連想配列

---
+++ sample84.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yanai
+ * Date: 2015/04/21
+ * Time: 16:22
+ */
+$fruits_mixed = array(
+    'peach',
+    'blueberry',
+    'apple' => 'red',
+    'banana' => 'yellow',
+    'orange' => 'orange',
+);
+
+echo $fruits_mixed[1],PHP_EOL;
+echo $fruits_mixed['banana'];
