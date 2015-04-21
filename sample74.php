aa3f9bd2b2ad5ce5f5bf34cbfcac55fec6bac0d3
Shingo Yanai <yanai@photocreate.co.jp>
2015-04-21 14:28:50 +0900

    [chap3 p74] 加算子・減算子

---
+++ sample74.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yanai
+ * Date: 2015/04/21
+ * Time: 14:09
+ */
+$a = 10;
+
+echo $a++,PHP_EOL;
+echo $a, PHP_EOL;
+echo ++$a,PHP_EOL;
+echo $a,PHP_EOL;
+echo $a--, PHP_EOL;
+echo $a,PHP_EOL;
+echo --$a,PHP_EOL;
+echo $a,PHP_EOL;
