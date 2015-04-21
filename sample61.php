137d6ef83f571fad18290457023d987837f8555c
Shingo Yanai <yanai@photocreate.co.jp>
2015-04-21 09:38:26 +0900

    [chap3 p61]文字列、ついでにvimの短形ヴィジュアルモードの練習

---
+++ sample61.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yanai
+ * Date: 2015/04/21
+ * Time: 9:18
+ */
+
+$string1 = 'this is string';
+$string2 = "this is string";
+$string3 = 'hi,$string2 \n';
+$string4 = "hi,$string2 \n";
+
+echo $string1,PHP_EOL;
+echo $string2,PHP_EOL;
+echo $string3,PHP_EOL;
+echo $string4,PHP_EOL;
+
+
