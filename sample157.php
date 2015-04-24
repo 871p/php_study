6c30fbcf25936ce8c9bc758f572f31f9ded591a7
Shingo Yanai <yanai@photocreate.co.jp>
2015-04-24 12:06:05 +0900

    [chap5 p159]例外

---
+++ sample157.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yanai
+ * Date: 2015/04/24
+ * Time: 11:36
+ */
+
+class ZeroDivisionException extends Exception{
+
+}
+
+function div ($v1,$v2)
+{
+    if ($v2 === 0){
+        throw new ZeroDivisionException("arg #2 is zero.");
+}
+    return $v1/$v2;
+}
+
+try {
+    echo div(1, 2), PHP_EOL;
+    echo div(1, 0), PHP_EOL;
+    echo div(2, 1), PHP_EOL;
+} catch (ZeroDivisionException $e){
+    echo 'Zero Division Exception!',PHP_EOL;
+    echo $e->getMessage(),PHP_EOL;
+}
