ff1dff6b392d23130c0248ca73a4b82cef9a1e5e
Shingo Yanai <yanai@photocreate.co.jp>
2015-04-30 10:25:29 +0900

    [いきなりはじめるPHP終わり]

--- phpkiso/thanks.php
+++ phpkiso/thanks.php
@@ -7,30 +7,48 @@
 <body
 
 <?php
-$nickname =$_POST['nickname'];
-$email =$_POST['email'];
-$goiken =$_POST['goiken'];
+try {
+    $dsn = 'mysql:dbname=phpkiso;host=study.localhost';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->query('SET NAMES utf8');
 
-$nickname = htmlspecialchars($nickname);
-$email = htmlspecialchars($email);
-$goiken = htmlspecialchars($goiken);
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $goiken = $_POST['goiken'];
 
-print $nickname;
-print '様 <br />';
-print 'ご意見ありがとうございました。<br />';
-print '頂いたご意見『';
-print $goiken;
-print '』<br />';
-print $email;
-print 'にメールを送りましたのでご確認ください。';
+    $nickname = htmlspecialchars($nickname);
+    $email = htmlspecialchars($email);
+    $goiken = htmlspecialchars($goiken);
 
-$mail_sub = 'アンケート受け付けました。';
-$mail_body = $nickname."様へ\nアンケートご協力ありがとうございました。";
-$mail_body = html_entity_decode($mail_body,ENT_QUOTES,"UTF-8");
-$mail_head = 'From: xxx@xxx.co.jp';
-mb_language('Japanese');
-mb_internal_encoding("UTF-8");
-mb_send_mail($email,$mail_sub,$mail_body,$mail_head);
+    print $nickname;
+    print '様 <br />';
+    print 'ご意見ありがとうございました。<br />';
+    print '頂いたご意見『';
+    print $goiken;
+    print '』<br />';
+    print $email;
+    print 'にメールを送りましたのでご確認ください。';
+
+    $mail_sub = 'アンケート受け付けました。';
+    $mail_body = $nickname . "様へ\nアンケートご協力ありがとうございました。";
+    $mail_body = html_entity_decode($mail_body, ENT_QUOTES, "UTF-8");
+    $mail_head = 'From: xxx@xxx.co.jp';
+    mb_language('Japanese');
+    mb_internal_encoding("UTF-8");
+    mb_send_mail($email, $mail_sub, $mail_body, $mail_head);
+
+    $sql = 'INSERT INTO anketo (nickname,email,goiken) VALUES ("' . $nickname . '","' . $email . '","' . $goiken . '")';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    $dbh = null;
+}
+catch(Exception $e){
+    print 'ただいま障害により大変ご迷惑をおかけしております。';
+}
+
 ?>
 
 
